d13b79a276274a44a3138d73f87c19aa0445bb3a
YangYangYang <57069586+JeromeYHj214@users.noreply.github.com>
2020-09-28 23:38:48 +0800

    1

---
+++ insert_sort.py
@@ -0,0 +1,15 @@
+def insert_sort(li):
+    for i in range(1, len(li)):
+        tmp = li[i];
+        j = i - 1
+        while j >= 0:
+            if li[j] > tmp:
+                li[j+1] = li[j]
+                j -= 1
+            else:
+                break
+        li[j+1] = tmp
+        print(li)
+
+li = [5,4,7,6,2,3,1]
+insert_sort(li)
